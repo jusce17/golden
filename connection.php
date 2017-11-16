2d604ce972d6cc509afaffd596b342ebdc68f216
Eden Juscelino <eden.juscelino@gmail.com>
2017-11-16 20:22:39 +0100

    connection script

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+$servername = "example";
+$username = "example";
+$password = "example";
+$dbname = "example";
+
+// create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+ ?>
